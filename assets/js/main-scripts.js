fd5ed960d6472276117d21a2ffd8f7361b8a52ca
Anna <annaverchenko86@gmail.com>
2016-05-22 16:55:17 +0300

    some changes index.jade

--- assets/js/main-scripts.js
+++ assets/js/main-scripts.js
@@ -4,6 +4,24 @@ Main JS functionality
 ===========================
 ***/
 
-$(document).ready(function() {
+//$(document).ready(function() {
+//
+//});
 
-});
+function show() {
+    var menu = document.getElementById('my');
+    my.style.display = 'block';
+}
+
+function hide() {
+    var menu = document.getElementById('my');
+    my.style.display = 'none';
+}
+
+function init() {
+    var menu = document.getElementById('my');
+    menu.onmouseover = show;
+    menu.onmouseout = hide;
+}
+
+init();
